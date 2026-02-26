0091daf85ab31cf92eb8deb00670b6d7c8386731
China-Nshen <51445659+China-Nshen@users.noreply.github.com>
2026-02-26 18:47:59 +0800

    feat(bukkit): run gp close on teleport completion

--- yuanluServerDo-bukkit/src/main/java/yuan/plugins/serverDo/bukkit/cmds/CmdWarp.java
+++ yuanluServerDo-bukkit/src/main/java/yuan/plugins/serverDo/bukkit/cmds/CmdWarp.java
@@ -4,13 +4,16 @@
 package yuan.plugins.serverDo.bukkit.cmds;
 
 import lombok.val;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import yuan.plugins.serverDo.Channel;
 import yuan.plugins.serverDo.Channel.Package.BoolConsumer;
+import yuan.plugins.serverDo.ShareData;
 import yuan.plugins.serverDo.Tool;
 import yuan.plugins.serverDo.bukkit.Core;
 import yuan.plugins.serverDo.bukkit.Main;
+import yuan.plugins.serverDo.bukkit.event.CrossServerTeleportEvent;
 
 import java.util.Collection;
 import java.util.function.BiConsumer;
@@ -37,8 +40,14 @@ public final class CmdWarp extends TabWarp {
 					msg("not-found", sender, arg);
 				} else {
 					msg("tp", sender, name, server);
+					ShareData.getLogger().info("[CrossServerTeleportEvent] WARP name=" + name + ", server=" + server + ", operator=" + player.getName());
+					Bukkit.getPluginManager().callEvent(new CrossServerTeleportEvent(player, player.getName(), "warp:" + name + "@" + server, null));
+					Main.openGermGui(player, "tpgui", "cross-server-teleport");
 					Core.listenCallBack(player, Channel.WARP, 3, (BoolConsumer) success -> {
-						if (!success) BC_ERROR.send(sender);
+						if (!success) {
+							BC_ERROR.send(sender);
+							Main.openGermGuiByCommand(player, "null", "basic.bungee-error");
+						} else Main.closeGermGuiByCommand(player, "cross-server-teleport-complete");
 					});
 					Core.BackHandler.recordLocation(player,server);
 					Main.send(player, Channel.Warp.s3C_tpWarp(name));
